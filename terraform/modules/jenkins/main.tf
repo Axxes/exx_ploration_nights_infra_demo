e8fa87718eed496abf0ab1fc09310b8a23d09cc1
Jelle De Boeck <jelle.deboeck@student.uantwerpen.be>
2023-02-26 23:38:50 +0100

    Added jenkins user data config to aws instance and added inbound 8080 traffic to access jenkins web page

--- terraform/modules/jenkins/main.tf
+++ terraform/modules/jenkins/main.tf
@@ -2,8 +2,22 @@ resource "aws_instance" "jenkins" {
   ami                = "ami-06e0ce9d3339cb039"
   instance_type      = "t2.micro"
   key_name           = aws_key_pair.ssh.key_name
-  vpc_security_group_ids = [aws_security_group.jenkins_allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.jenkins_allow_ssh.id, aws_security_group.jenkins_allow_http.id]
   subnet_id          = aws_subnet.main.id
+
+  user_data_replace_on_change = true
+  user_data = <<EOF
+#!/bin/bash
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade -y
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+EOF
+
   tags               = merge(var.default_tags,
     { Name = "jenkins_instance" })
 }
@@ -19,18 +33,46 @@ resource "aws_security_group" "jenkins_allow_ssh" {
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
   tags = merge(var.default_tags,
     { Name = "jenkins_allow_ssh" })
+}
+
+resource "aws_security_group" "jenkins_allow_http" {
+  name        = "jenkins_allow_http"
+  description = "Allow http inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = merge(var.default_tags,
+    { Name = "jenkins_allow_http" })
 }
 
 resource "aws_eip" "jenkins_public_ip" {
