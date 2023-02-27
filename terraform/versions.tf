2be2b33f26104c4aa6cbf25cfbf789c27730fe31
Jelle De Boeck <jelle.deboeck@axxes.com>
2023-02-27 17:15:02 +0100

    Added S3 backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,4 +1,13 @@
 terraform {
+  backend "s3" {
+    bucket         = "axxes-cloud-workshop-sandbox-2023-tfstate"
+    key            = "exxploration_nights/terraform.tfstate"
+    region         = "eu-west-1"
+    encrypt        = true
+    dynamodb_table = "axxes-cloud-workshop-sandbox-2023-tfstate-lock"
+    profile        = "workshop-sandbox-2023"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
