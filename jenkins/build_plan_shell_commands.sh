179549a30d4c0884b272a1860c5d5ab33aba387b
Jelle De Boeck <jelle.deboeck@student.uantwerpen.be>
2023-03-03 20:38:07 +0100

    Had to copy both files in both vms, because image GET request can go to each aws instance

--- jenkins/build_plan_shell_commands.sh
+++ jenkins/build_plan_shell_commands.sh
@@ -2,5 +2,7 @@ pwd
 ls
 scp -i /var/lib/jenkins/keys/web_id_rsa_ssh -o StrictHostKeyChecking=no /var/lib/jenkins/templates/index_1.html ec2-user@46.137.7.126:/var/www/html/index.html
 scp -i /var/lib/jenkins/keys/web_id_rsa_ssh -o StrictHostKeyChecking=no /var/lib/jenkins/templates/traineeship_image_1.png ec2-user@46.137.7.126:/var/www/html/traineeship_image_1.png
+scp -i /var/lib/jenkins/keys/web_id_rsa_ssh -o StrictHostKeyChecking=no /var/lib/jenkins/templates/traineeship_image_2.png ec2-user@46.137.7.126:/var/www/html/traineeship_image_2.png
 scp -i /var/lib/jenkins/keys/web_id_rsa_ssh -o StrictHostKeyChecking=no /var/lib/jenkins/templates/index_2.html ec2-user@54.216.112.4:/var/www/html/index.html
+scp -i /var/lib/jenkins/keys/web_id_rsa_ssh -o StrictHostKeyChecking=no /var/lib/jenkins/templates/traineeship_image_1.png ec2-user@54.216.112.4:/var/www/html/traineeship_image_1.png
 scp -i /var/lib/jenkins/keys/web_id_rsa_ssh -o StrictHostKeyChecking=no /var/lib/jenkins/templates/traineeship_image_2.png ec2-user@54.216.112.4:/var/www/html/traineeship_image_2.png
