de439c3ebd046cb58042d6a0f91b29d76633e636
Lorenzo De Bie <lorenzo.debie@axxes.com>
2023-03-07 17:34:05 +0100

    rename of loadbalancer parts for main branch

--- terraform/modules/loadbalanced_vms/main.tf
+++ terraform/modules/loadbalanced_vms/main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "web_allow_http" {
 }
 
 resource "aws_lb_target_group" "web_lb_tg" {
-  name     = "web-lb-tg"
+  name     = "web-lb-tg-main"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
@@ -81,7 +81,7 @@ resource "aws_lb_target_group_attachment" "web_lb_tg_attachment" {
 }
 
 resource "aws_lb" "web_lb" {
-  name               = "web-lb"
+  name               = "web-lb-main"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web_lb_security_group.id]
